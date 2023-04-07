98172d94872504bc59a1822f82d82e78b3ed5ff7
Heiner000 <garrett.heiner@gmail.com>
2023-04-07 16:42:02 -0700

    models created, pre-migration

---
+++ migrations/20230407233659-create-movie.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('movies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.INTEGER
+      },
+      director: {
+        type: Sequelize.STRING
+      },
+      plot: {
+        type: Sequelize.TEXT
+      },
+      imdbID: {
+        type: Sequelize.STRING
+      },
+      poster: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('movies');
+  }
+};
